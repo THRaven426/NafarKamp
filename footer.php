8eb98c32ce6ccbcdd55711032ef41700cfaed1f8
Harrytx426 <30800174+Harrytx426@users.noreply.github.com>
2023-01-24 11:59:16 +0100

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="./assets/css/footer.css" rel="stylesheet">
+    <title>Document</title>
+</head>
+
+<body>
+    <div id="footer">
+        <div id="izquierda">
+            <img src="./assets/imagenes/NafarKampSBCK.png" alt="Logo NafarKamp">
+        </div>
+        <div class="enlaces">
+            <div>
+                <h4>NAFARKAMP</h4>
+                <br>
+                <a href="">Sobre Nosotros</a> <br>
+                <a href="">Conocenos</a> <br>
+                <a href="">Politica de privacidad</a> <br>
+            </div>
+        </div>
+        <div class="enlaces">
+            <div>
+                <h4>RESERVA AHORA</h4>
+                <br>
+                <a href="">Inicio</a> <br>
+                <a href="">Bungalows</a> <br>
+                <a href="">Reserva ahora</a> <br>
+            </div>
+        </div>
+        <div class="enlaces">
+            <div>
+                <h4>OTROS</h4>
+                <br>
+                <a href="">Intranet</a> <br>
+                <a href="">Donde encontranos</a> <br>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet" />
+    <!-- Google Web Fuentes -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
+
+
+    <link href="./assets/css/styles.css" rel="stylesheet">
+    <title>NAFAR KAMP | Inicio</title>
+</head>
+<body>
+    <div class="arriba1">
+        <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+            <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" class="relleno_arriba"></path>
+        </svg>
+    </div>
+    <div class="container">
+    <nav class="navbar container">
+        <a id="nafarnav" href="#" ><b>NAFAR<span class="color-green">KAMP</span></b></a>
+        <input type="checkbox" id="toggler">
+        <label for="toggler"><i class="ri-menu-line"></i></label>
+        <div class="menu">
+            <ul class="list">
+                <li><a href="">Inicio</a></li>
+                <li><a href="">Bungalows</a></li>
+                <li><a href="">Reservas</a></li>
+                <li><a href="">Sobre nosotros</a></li>
+                <li class="ini_ses"><a href="">Iniciar Sesión</a></li>
+            </ul>
+        </div>
+    </nav>
+    </div>
+    <div id="zona_superior">
+        
+        <div class="arribatxt">
+            <h1>NAFARKAMP</h1>
+            <h2>Reserva ya tu bungalow</h2>
+        </div>
+        <a href="#"><button id="boton_reserva"><b>Reserva ahora</a></b></button>
+    </div>
+    <div id="slide-bunw">
+    <input type="radio" name="slider" id="s1" checked />
+    <input type="radio" name="slider" id="s2" />
+    <input type="radio" name="slider" id="s3" />
+    <input type="radio" name="slider" id="s4" />
+    <input type="radio" name="slider" id="s5" />
+    <label for="s1" id="slide1">
+        <img src="./assets/imagenes/camping-norte1-grande.png" alt />
+    </label>
+    <label for="s2" id="slide2">
+        <img src="./assets/imagenes/camping-norte2-pequeño.png" alt />
+    </label>
+    <label for="s3" id="slide3">
+        <img src="./assets/imagenes/camping-sur1-pequeño.png" alt />
+    </label>
+    <label for="s4" id="slide4">
+        <img src="./assets/imagenes/camping-sur2-mediano.png" alt />
+    </label>
+    <label for="s5" id="slide5">
+        <img src="./assets/imagenes/camping-sur3-mediano.png" alt />
+    </label>
+    </div>
+    <?php
+    include('footer.php');
+    ?>
+</body>
+</html>
